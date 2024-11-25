4e31313b318297cae5aaa623608fe62688b48245
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2024-11-24 19:39:10 -0500

    test new render for svelte 5

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -1,40 +1,36 @@
-import { convert } from 'html-to-text';
-import pretty from 'pretty';
-import type { ComponentProps, ComponentType, SvelteComponent } from 'svelte';
-
-export const render = <Component extends SvelteComponent>({
-	template,
-	props,
-	options
-}: {
-	template: ComponentType<Component>;
-	props?: ComponentProps<Component>;
-	options?: {
-		plainText?: boolean;
-		pretty?: boolean;
-	};
-}) => {
-	const { html } =
-		// @ts-ignore
-		template.render(props);
-	if (options?.plainText) {
-		return renderAsPlainText(html);
-	}
-	const doctype =
-		'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
-	const markup = html;
-	const document = `${doctype}${markup}`;
-	if (options?.pretty) {
-		return pretty(document);
-	}
-	return document;
-};
+import type { Component, ComponentProps, SvelteComponent } from "svelte";
+import { convert } from "html-to-text";
+import { render } from "svelte/server";
 
 const renderAsPlainText = (markup: string) => {
-	return convert(markup, {
-		selectors: [
-			{ selector: 'img', format: 'skip' },
-			{ selector: '#__svelte-email-preview', format: 'skip' }
-		]
-	});
+  return convert(markup, {
+    selectors: [
+      { selector: "img", format: "skip" },
+      { selector: "#__svelte-email-preview", format: "skip" },
+    ],
+  });
+};
+
+export const renderEmail = async <
+  Comp extends SvelteComponent<any> | Component<any>,
+  Props extends ComponentProps<Comp> = ComponentProps<Comp>,
+>(
+  component: Comp,
+  props?: Props,
+) => {
+  const rendered = render(component as any, {
+    props,
+  });
+
+  const doctype =
+    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+
+  const html = `${doctype}${rendered.body}`;
+
+  const text = renderAsPlainText(rendered.body);
+
+  return {
+    html,
+    text,
+  };
 };
